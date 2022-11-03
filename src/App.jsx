6f8013dc417b2e2f4cddd7e828b2f6d073750834
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-03 14:20:02 +0300

    21. React Router v6. useRoutes()

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useLocation, useRoutes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import AuthLayout from "./layouts/AuthLayout";
-import PostsLayout from "./layouts/PostsLayout";
-import MainPage from "./pages/MainPage";
 import NavBar from "./components/NavBar/NavBar";
-// import ProtectedRoute from "./components/ProtectedRoute";
 import withRedux from "./hoc/withRedux";
 import withRouter from "./hoc/withRouter";
+import routes from "./routes";
 import "react-toastify/dist/ReactToastify.css";
-import PostPage from "./pages/Posts/PostPage"
-import PostsListPage from "./pages/Posts/PostsListPage"
-import LoginPage from "./pages/LoginPage"
-import SignUpPage from "./pages/SigupPage"
-import ProtectedRoute from "./components/ProtectedRoute";
+import { isLoggedInSelector } from "./store/authSlice";
 
 function App() {
+    const isLoggedIn = useSelector(isLoggedInSelector());
+    const location = useLocation();
+    const elements = useRoutes(routes(isLoggedIn, location));
+
     return (
         <div className='min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col'>
             <NavBar />
-            <Routes>
-                <Route index element={<MainPage />} />
-                <Route path='auth' element={<AuthLayout />}>
-                    <Route index element={<Navigate to="/auth/signup" />} />
-                    <Route path={"login"} element={<LoginPage />} />
-                    <Route path={"signup"} element={<SignUpPage />} />
-                    <Route path="*" element={<Navigate to="/auth/signup" />} />
-                </Route>
-                <Route path='posts' element={<ProtectedRoute redirectTo={"/auth/login"} element={<PostsLayout />} />}>
-                    <Route path=":postId" element={<PostPage />} />
-                    <Route index element={<PostsListPage />} />
-                </Route>
-                <Route path="*" element={<Navigate to='posts' />} />
-            </Routes>
-
+            {elements}
             <ToastContainer />
         </div>
-    );
+    )
 }
+
 const AppWithStoreAndRoutes = withRedux(withRouter(App));
+
 export default AppWithStoreAndRoutes;
