b428e1a33a2bfe5ce742973d166470afafa1c59a
Kettengeschwindigkeit <666damned999@mail.ru>
2022-11-05 10:25:27 +0300

    21. React Router v6. Homework. Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { NavLink, Redirect, Route, Switch } from "react-router-dom";
+import { Navigate, NavLink, Route, Routes, useParams } from "react-router-dom";
 
 function HomePage() {
     return (
         <div className="m-4">
             <h1 className="text-3xl font-bold">Home page</h1>
-            <NavLink to="/users" className="text-lg hover:text-blue-700">Users list page</NavLink>
+            <NavLink to="users" className="text-lg hover:text-blue-700">Users list page</NavLink>
         </div>
     )
 }
@@ -16,18 +16,18 @@ function UsersListPage() {
             <h1 className="text-3xl font-bold">Users List Page</h1>
             <NavLink to="/" className="text-lg hover:text-blue-700">Main page</NavLink>
             <ul className="text-sm">
-                <li><NavLink to="users/1" className="hover:text-blue-700">User 1</NavLink></li>
-                <li><NavLink to="users/2" className="hover:text-blue-700">User 2</NavLink></li>
-                <li><NavLink to="users/3" className="hover:text-blue-700">User 3</NavLink></li>
-                <li><NavLink to="users/4" className="hover:text-blue-700">User 4</NavLink></li>
-                <li><NavLink to="users/5" className="hover:text-blue-700">User 5</NavLink></li>
+                <li><NavLink to="1" className="hover:text-blue-700">User 1</NavLink></li>
+                <li><NavLink to="2" className="hover:text-blue-700">User 2</NavLink></li>
+                <li><NavLink to="3" className="hover:text-blue-700">User 3</NavLink></li>
+                <li><NavLink to="4" className="hover:text-blue-700">User 4</NavLink></li>
+                <li><NavLink to="5" className="hover:text-blue-700">User 5</NavLink></li>
             </ul>
         </div>
     )
 }
 
-function UserPage({ match }) {
-    const id = match.params.id
+function UserPage() {
+    const params = useParams()
 
     return (
         <div className="m-4">
@@ -36,24 +36,24 @@ function UserPage({ match }) {
                 <NavLink to="/users" className="text-lg hover:text-blue-700">Users list page</NavLink>
             </div>
             <div>
-                <NavLink to={`/users/${id}/edit`} className="text-lg hover:text-blue-700">Edit user page</NavLink>
+                <NavLink to={`/users/${params.id}/edit`} className="text-lg hover:text-blue-700">Edit user page</NavLink>
             </div>
-            <div>userId: {id}</div>
+            <div>userId: {params.id}</div>
         </div>
     )
 }
 
-function UserEditPage({ match }) {
-    const id = match.params.id
+function UserEditPage() {
+    const params = useParams()
 
     return (
         <div className="m-4">
             <h1 className="text-3xl font-bold">User Edit Page</h1>
             <div>
-                <NavLink to={`/users/${id}`} className="text-lg hover:text-blue-700">Users profile page</NavLink>
+                <NavLink to={`/users/${params.id}`} className="text-lg hover:text-blue-700">Users profile page</NavLink>
             </div>
             <div>
-                <NavLink to={`/users/${+id + 1}`} className="text-lg hover:text-blue-700">Another user</NavLink>
+                <NavLink to={`/users/${+params.id + 1}`} className="text-lg hover:text-blue-700">Another user</NavLink>
             </div>
             <div>
                 <NavLink to="/users" className="text-lg hover:text-blue-700">Users list page</NavLink>
@@ -64,13 +64,13 @@ function UserEditPage({ match }) {
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/users/:id/edit" component={UserEditPage} />
-            <Route path="/users/:id" component={UserPage} />
-            <Route path="/users" component={UsersListPage} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="users" element={<UsersListPage />} />
+            <Route path="users/:id" element={<UserPage />} />
+            <Route path="users/:id/edit" element={<UserEditPage />} />
+            <Route path="*" element={<Navigate to="" />} />
+        </Routes>
     )
 }
 
